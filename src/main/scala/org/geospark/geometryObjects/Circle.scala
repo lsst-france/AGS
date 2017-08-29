a9557613a68af19a8ab6674ae1ebf75614eb2d96
Arnault <arnault@lal.in2p3.fr>
2017-08-29 10:17:42 +0200

    Add gitignore Update Circle.scala

--- src/main/scala/org/geospark/geometryObjects/Circle.scala
+++ src/main/scala/org/geospark/geometryObjects/Circle.scala
@@ -1,64 +1,57 @@
 package org.geospark.geometryObjects
 
-import com.vividsolutions.jts.geom._
+import com.vividsolutions.jts.geom.Coordinate
+import com.vividsolutions.jts.geom.CoordinateFilter
+import com.vividsolutions.jts.geom.CoordinateSequenceComparator
+import com.vividsolutions.jts.geom.CoordinateSequenceFilter
+import com.vividsolutions.jts.geom.Envelope
+import com.vividsolutions.jts.geom.Geometry
+import com.vividsolutions.jts.geom.GeometryComponentFilter
+import com.vividsolutions.jts.geom.GeometryFactory
+import com.vividsolutions.jts.geom.GeometryFilter
 
 
-// TODO: Auto-generated Javadoc
-/**
-  * The Class Circle.
-  */
-class Circle extends Geometry(new GeometryFactory()) {
-  var centerGeometry: Geometry
-  val givenRadius: Double
+class Circle(var centerGeometry: Geometry, val givenRadius: Double) extends Geometry(new GeometryFactory(centerGeometry.getPrecisionModel)) {
 
-  /** The center point. */
   private var centerPoint: Coordinate = null
-  /** The radius. */
-  private var radius: Double = .0
-  /** The mbr. */
+  private var radius = .0
   private var MBR: Envelope = null
 
-  def getCenterGeometry = centerGeometry
-  def getCenterPoint = centerPoint
-  def getRadius = radius
+  def getCenterPoint: Coordinate = this.centerPoint
+  def getRadius: Double = radius
 
-  def this(centerGeometry: Geometry, givenRadius: Double) {
-    // this(new GeometryFactory)
-    // super.(new PrecisionModel(centerGeometry.getPrecisionModel))
-    this.centerGeometry = centerGeometry
+  val centerGeometryMBR: Envelope = this.centerGeometry.getEnvelopeInternal
 
-    val centerGeometryMBR = this.centerGeometry.getEnvelopeInternal
-    this.centerPoint = new Coordinate(
-      (centerGeometryMBR.getMinX + centerGeometryMBR.getMaxX) / 2.0,
-      (centerGeometryMBR.getMinY + centerGeometryMBR.getMaxY) / 2.0)
-    // Get the internal radius of the object. We need to make sure that the circle at least should be the minimum circumscribed circle
-    val width = centerGeometryMBR.getMaxX - centerGeometryMBR.getMinX
-    val length = centerGeometryMBR.getMaxY - centerGeometryMBR.getMinY
-    val centerGeometryInternalRadius = Math.sqrt(width * width + length * length) / 2
-    this.radius = if (givenRadius > centerGeometryInternalRadius) givenRadius else centerGeometryInternalRadius
-    this.MBR = new Envelope(
-      this.centerPoint.x - this.radius,
-      this.centerPoint.x + this.radius,
-      this.centerPoint.y - this.radius,
-      this.centerPoint.y + this.radius)
-    this.setUserData(centerGeometry.getUserData)
-  }
+  this.centerPoint = new Coordinate(
+    (centerGeometryMBR.getMinX + centerGeometryMBR.getMaxX) / 2.0,
+    (centerGeometryMBR.getMinY + centerGeometryMBR.getMaxY) / 2.0)
 
-  def setRadius(givenRadius: Double): Unit = { // Get the internal radius of the object. We need to make sure that the circle at least should be the minimum circumscribed circle
+  // Get the internal radius of the object. We need to make sure that the circle at least should be the minimum circumscribed circle
+  val width: Double = centerGeometryMBR.getMaxX - centerGeometryMBR.getMinX
+  val length: Double = centerGeometryMBR.getMaxY - centerGeometryMBR.getMinY
+  val centerGeometryInternalRadius: Double = Math.sqrt(width * width + length * length) / 2
+  this.radius = if (givenRadius > centerGeometryInternalRadius) givenRadius
+  else centerGeometryInternalRadius
+  this.MBR = new Envelope(
+    this.centerPoint.x - this.radius,
+    this.centerPoint.x + this.radius,
+    this.centerPoint.y - this.radius,
+    this.centerPoint.y + this.radius)
+  this.setUserData(centerGeometry.getUserData)
+
+  def getCenterGeometry: Geometry = centerGeometry
+
+  def setRadius(givenRadius: Double): Unit = {
     val centerGeometryMBR = this.centerGeometry.getEnvelopeInternal
     val width = centerGeometryMBR.getMaxX - centerGeometryMBR.getMinX
     val length = centerGeometryMBR.getMaxY - centerGeometryMBR.getMinY
     val centerGeometryInternalRadius = Math.sqrt(width * width + length * length) / 2
     this.radius = if (givenRadius > centerGeometryInternalRadius) givenRadius
     else centerGeometryInternalRadius
-    this.MBR = new Envelope(
-      this.centerPoint.x - this.radius,
-      this.centerPoint.x + this.radius,
-      this.centerPoint.y - this.radius,
-      this.centerPoint.y + this.radius)
+    this.MBR = new Envelope(this.centerPoint.x - this.radius, this.centerPoint.x + this.radius, this.centerPoint.y - this.radius, this.centerPoint.y + this.radius)
   }
 
-  def covers(g: Geometry): Boolean = { // short-circuit test
+  override def covers(g: Geometry): Boolean = { // short-circuit test
     if (!getEnvelopeInternal.covers(g.getEnvelopeInternal)) return false
     // optimization for rectangle arguments
     if (isRectangle) { // since we have already tested that the test envelope is covered
@@ -75,12 +68,9 @@ class Circle extends Geometry(new GeometryFactory()) {
     covers(x1, y1) && covers(x1, y2) && covers(x2, y2) && covers(x2, y1)
   }
 
-  def intersects(g: Geometry): Boolean = { // short-circuit test
+  override def intersects(g: Geometry): Boolean = {
     if (!getEnvelopeInternal.covers(g.getEnvelopeInternal)) return false
-    // optimization for rectangle arguments
-    if (isRectangle) { // since we have already tested that the test envelope is covered
-      return true
-    }
+    if (isRectangle) return true
     var x1 = .0
     var x2 = .0
     var y1 = .0
@@ -93,71 +83,55 @@ class Circle extends Geometry(new GeometryFactory()) {
   }
 
   def covers(x: Double, y: Double): Boolean = {
-    val distance = Math.sqrt(
-      (x - this.centerPoint.x) * (x - this.centerPoint.x) +
-        (y - this.centerPoint.y) * (y - this.centerPoint.y))
+    val distance = Math.sqrt((x - this.centerPoint.x) * (x - this.centerPoint.x) + (y - this.centerPoint.y) * (y - this.centerPoint.y))
     if (distance <= this.radius) true
     else false
   }
 
-  def getGeometryType = "Circle"
-  def getCoordinate = centerGeometry.getCoordinate
-  def getCoordinates = centerGeometry.getCoordinates
-  def getNumPoints = centerGeometry.getNumPoints
-  def isEmpty = centerGeometry.isEmpty
-  def getDimension = 0
-  def getBoundary = getFactory.createGeometryCollection(null)
-  def getBoundaryDimension = 0
-
-  def reverse = {
-    val g = centerGeometry.reverse
-    val newCircle = new Circle(g, radius)
+  override def getGeometryType = "Circle"
+  override def getCoordinate: Coordinate = this.centerGeometry.getCoordinate
+  override def getCoordinates: Array[Coordinate] = this.centerGeometry.getCoordinates
+  override def getNumPoints: Int = this.centerGeometry.getNumPoints
+  override def isEmpty: Boolean = this.centerGeometry.isEmpty
+  override def getDimension = 0
+  override def getBoundary: Geometry = getFactory.createGeometryCollection(null)
+  override def getBoundaryDimension = 0
+  override def reverse: Geometry = {
+    val g = this.centerGeometry.reverse
+    val newCircle = new Circle(g, this.radius)
     newCircle
   }
 
-  def clone = {
-    val g = centerGeometry.clone.asInstanceOf[Geometry]
-    val cloneCircle = new Circle(g, radius)
-    cloneCircle
+  override def clone: Geometry = {
+    val g = this.centerGeometry.clone.asInstanceOf[Geometry]
+    val cloneCircle = new Circle(g, this.radius)
+    cloneCircle // return the clone
   }
 
-  def equalsExact(g: Geometry, tolerance: Double): Boolean = {
-    val type1 = getGeometryType
-    val type2 = g.getGeometryType
+  override def equalsExact(g: Geometry, tolerance: Double): Boolean = {
+    val type1 = this.getGeometryType
+    val type2 = g.asInstanceOf[Geometry].getGeometryType
     val radius1 = this.radius
     val radius2 = g.asInstanceOf[Circle].radius
     if (type1 ne type2) return false
     if (radius1 != radius2) return false
-    centerGeometry.equals(g.asInstanceOf[Circle].centerGeometry)
+    this.centerGeometry == g.asInstanceOf[Circle].centerGeometry
   }
 
-  def apply(filter: CoordinateFilter): Unit = {
-    // Do nothing. This circle is not expected to be a complete geometry.
-  }
+  override def apply(filter: CoordinateFilter): Unit = {}
+  override def apply(filter: CoordinateSequenceFilter): Unit = {}
+  override def apply(filter: GeometryFilter): Unit = {}
+  override def apply(filter: GeometryComponentFilter): Unit = {}
+  override def normalize(): Unit = {}
 
-  def apply(filter: CoordinateSequenceFilter): Unit = {
-    // Do nothing. This circle is not expected to be a complete geometry.
-  }
-
-  def apply(filter: GeometryFilter): Unit = {
-    // Do nothing. This circle is not expected to be a complete geometry.
-  }
-
-  def apply(filter: GeometryComponentFilter): Unit = {
-    // Do nothing. This circle is not expected to be a complete geometry.
-  }
-
-  def normalize(): Unit = {
-  }
-
-  protected def computeEnvelopeInternal: Envelope = {
+  override protected def computeEnvelopeInternal: Envelope = {
     if (isEmpty) return new Envelope
-    MBR
+    this.MBR
   }
 
-  protected def compareToSameClass(other: Any): Int = {
+  override protected def compareToSameClass(other: Any): Int = {
     val env = other.asInstanceOf[Envelope]
-    val mbr = MBR
+    val mbr = this.MBR
     // compare based on numerical ordering of ordinates
     if (mbr.getMinX < env.getMinX) return -1
     if (mbr.getMinX > env.getMinX) return 1
@@ -170,10 +144,9 @@ class Circle extends Geometry(new GeometryFactory()) {
     0
   }
 
-  protected def compareToSameClass(other: Any, comp: CoordinateSequenceComparator): Int = {
+  override protected def compareToSameClass(other: Any, comp: CoordinateSequenceComparator): Int = {
     val env = other.asInstanceOf[Envelope]
-    val mbr = MBR
-    // compare based on numerical ordering of ordinates
+    val mbr = this.MBR
     if (mbr.getMinX < env.getMinX) return -1
     if (mbr.getMinX > env.getMinX) return 1
     if (mbr.getMinY < env.getMinY) return -1
