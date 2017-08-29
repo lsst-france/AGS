aa2652698b8f979f73100da2fd81347ad79252d6
Arnault <arnault@lal.in2p3.fr>
2017-08-29 11:25:33 +0200

    Try scalatest

--- src/test/scala/org/geospark/App.scala
+++ src/test/scala/org/geospark/App.scala
@@ -1,10 +1,15 @@
 package org.geospark
 
+import com.vividsolutions.jts.geom.{Coordinate, Envelope, GeometryFactory}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
 import org.geospark.enums._
 import org.geospark.enums.{FileDataSplitter, GridType, IndexType}
 import org.geospark.formatMapper.EarthdataHDFPointMapper
 import org.geospark.spatialOperator.{JoinQuery, KNNQuery, RangeQuery}
 import org.geospark.spatialRDD.{CircleRDD, PointRDD, PolygonRDD}
+import org.scalatest.FunSpec
 
 object App {
   def time[R](text: String, block: => R): R = {
@@ -28,5 +33,42 @@ object App {
   def main (arg: Array[String]) = {
     println( "Hello World!" )
     println(test)
+
+    val conf = new SparkConf().setAppName("scalaTest").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    // val resourceFolder = System.getProperty("user.dir")+"/src/test/resources/"
+    val resourceFolder = "/home/christian.arnault/geospark/GeoSpark/core/src/test/resources/"
+
+    val PointRDDInputLocation = resourceFolder+"arealm-small.csv"
+    val PointRDDSplitter = FileDataSplitter.CSV
+    val PointRDDIndexType = IndexType.RTREE
+    val PointRDDNumPartitions = 5
+    val PointRDDOffset = 0
+
+    val PolygonRDDInputLocation = resourceFolder + "primaryroads-polygon.csv"
+    val PolygonRDDSplitter = FileDataSplitter.CSV
+    val PolygonRDDNumPartitions = 5
+    val PolygonRDDStartOffset = 0
+    val PolygonRDDEndOffset = 8
+
+    val geometryFactory=new GeometryFactory()
+    val kNNQueryPoint=geometryFactory.createPoint(new Coordinate(-84.01, 34.01))
+    val rangeQueryWindow=new Envelope (-90.01,-80.01,30.01,40.01)
+    val joinQueryPartitioningType = GridType.RTREE
+    val eachQueryLoopTimes=1
+
+    /*
+    it("should pass spatial range query") {
+      val objectRDD = new PointRDD(sc, PointRDDInputLocation, PointRDDOffset, PointRDDSplitter, true, StorageLevel.MEMORY_ONLY)
+      for(i <- 1 to eachQueryLoopTimes)
+      {
+        val resultSize = RangeQuery.SpatialRangeQuery(objectRDD, rangeQueryWindow, false,false).count
+      }
+    }
+*/
+
   }
 }
---
+++ src/test/scala/org/geospark/tgs.scala
@@ -0,0 +1,13 @@
+package org.geospark
+
+import org.scalatest.FunSpec
+
+class tgs extends FunSpec {
+  describe("GeoSpark in Scala") {
+    it("should pass spatial range query") {
+    }
+
+  }
+}
+
+
